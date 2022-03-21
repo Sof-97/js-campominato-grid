99494ce17e799c3c39fe368fc5c2aff44d7b3f43
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-03-21 17:28:20 +0100

    Prettier

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,9 @@ let container = document.getElementById('grid');
 let mode = 0;
 //Click su play
 document.getElementById('start').addEventListener('click',
-    function(){
+    function () {
         let levels = document.getElementById('levels').value;
-        switch (levels){
+        switch (levels) {
             case "uno":
                 mode = 100;
                 document.documentElement.style.setProperty('--column', '10');
@@ -22,19 +22,19 @@ document.getElementById('start').addEventListener('click',
         container.innerHTML = '';
         //Box creation
         box(mode)
-    }    
+    }
 )
 
-function box(a){
-    for(i = 1; i<=a; i++){
+function box(a) {
+    for (i = 1; i <= a; i++) {
         let box = document.createElement('div');
         box.classList.add('box');
         box.innerHTML = i;
         container.appendChild(box);
         box.addEventListener('click',
-             function(){
-                 this.classList.add('clicked');
-             }
+            function () {
+                this.classList.add('clicked');
+            }
         )
     }
 }

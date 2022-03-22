b6e33d9fe43792f19414dec26998975b453b7490
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-03-22 11:51:00 +0100

    Added shuffle

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,10 +26,21 @@ document.getElementById('start').addEventListener('click',
 )
 
 function box(a) {
+    let array = [];
+    for (i = 0; i < a; i++) {
+        array.push(i);
+    }
+    console.log(array)
+    //Swap numeri random
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    console.log(array)
     for (i = 1; i <= a; i++) {
         let box = document.createElement('div');
         box.classList.add('box');
-        box.innerHTML = i;
+        box.innerHTML = array[i - 1];
         container.appendChild(box);
         box.addEventListener('click',
             function () {
@@ -37,4 +48,9 @@ function box(a) {
             }
         )
     }
+}
+
+function numGen(z) {
+    x = (Math.floor(Math.random() * z) + 1);
+    return x;
 }
